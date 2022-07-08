99bd654d1e9cf68fb5ed45c8c194df0767685ce7
unknown <moonklacy@gmail.com>
2022-07-08 10:04:08 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+function hiatus(){
+    alert("maaf fitur ini belum selesai dibuat, mohon jelajahi main website portofolio nabila terlebih dahulu ^-^")
+}
+
+function explore(){
+  document.querySelector('#space').scrollIntoView({
+        behavior: 'smooth'
+      });
+setTimeout(function(){
+  document.querySelector('#space1').scrollIntoView({
+        behavior: 'smooth'
+      });
+  setTimeout(function(){
+  document.querySelector('#space2').scrollIntoView({
+        behavior: 'smooth'
+      });
+    setTimeout(function(){
+  document.querySelector('#b7').scrollIntoView({
+        behavior: 'smooth'
+      });
+      setTimeout(function(){
+  document.querySelector('#space6').scrollIntoView({
+        behavior: 'smooth'
+      });
+}, 2000); 
+}, 2000); 
+}, 2000); 
+}, 2000); 
+}
+
+function wushAbout(){
+    document.querySelector('#space').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+function wushSkill(){
+    document.querySelector('#space1').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+function wushEdu(){
+    document.querySelector('#space2').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+function wushAcv(){
+    document.querySelector('#space3').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+function wushProject(){
+    document.querySelector('#b7').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+function wushArt(){
+    document.querySelector('#space5').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+function wushContacts(){
+    document.querySelector('#space6').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
